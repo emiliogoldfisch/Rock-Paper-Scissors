9d1685b37b6e91c829741ef87626d2b3a284c359
emiliogoldfisch <havietduc@hotmail.de>
2022-05-20 20:40:16 +0200

    Write playRound function.

--- javascript.js
+++ javascript.js
@@ -113,26 +113,38 @@ function playRound(computerSelection, playerSelection) {
     playerSelection = playerSelection();
 
     // determine who the winner is;
-    //     if playerSelection == 'rock'
-    //         if computerSelection == 'rock'
-    //             console.log('Draw! Rock vs rock!');
-    //         if computerSelection == 'paper'
-    //             console.log('You lose! Paper beats rock!');             
-    //         if computerSelection == 'scissors'
-    //             console.log('You win! Rock beats scissors!');
-    //     if playerSelection == 'paper'
-    //         if computerSelection == 'rock'
-    //             console.log('You win! Paper beats rock!');
-    //         if computerSelection == 'paper'
-    //             console.log('Draw! Paper vs paper!');             
-    //         if computerSelection == 'scissors'
-    //             console.log('You lose! Scissors beats paper!');
-    //     if playerSelection == 'scissors'
-    //         if computerSelection == 'rock'
-    //             console.log('You lose! Rock beats scissors!');
-    //         if computerSelection == 'paper'
-    //             console.log('You win! Scissors beats paper!');             
-    //         if computerSelection == 'scissors'
-    //             console.log('Draw! Scissors vs scissors!');
+    if (playerSelection == 'rock') {
+        if (computerSelection == 'rock') {
+            console.log('Draw! Rock vs rock!');
+        }
+        else if (computerSelection == 'paper') {
+            console.log('You lose! Paper beats rock!');  
+        }
+        else if (omputerSelection == 'scissors') {
+            console.log('You win! Rock beats scissors!');
+        }
+    }
+    else if (playerSelection == 'paper') {
+        if (computerSelection == 'rock') {
+            console.log('You win! Paper beats rock!');
+        }
+        else if (computerSelection == 'paper') {
+            console.log('Draw! Paper vs paper!');  
+        }
+        else if (computerSelection == 'scissors') {
+            console.log('You lose! Scissors beats paper!');
+        }
+    }
+    else if (playerSelection == 'scissors') {
+        if (computerSelection == 'rock') {
+            console.log('You lose! Rock beats scissors!');
+        }
+        else if (computerSelection == 'paper') {
+            console.log('You win! Scissors beats paper!');
+        }
+        else if (computerSelection == 'scissors') {
+            console.log('Draw! Scissors vs scissors!');
+        }
+    }
 
 }
